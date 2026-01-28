b1cd455c3a8febf0914e4d0d31ba644f423a5d04
Subhiksha-gaviya <subhigaviya2005_bit27@mepcoeng.ac.in>
2026-01-28 12:21:30 +0530

    Add files via upload

---
+++ hackerrank.java
@@ -0,0 +1,131 @@
+1)
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+class Animal{
+	void walk(){
+		System.out.println("I am walking");
+	}
+}
+
+class Bird extends Animal{
+	void fly(){
+		System.out.println("I am flying");
+	}
+    void sing(){
+        System.out.println("I am singing");
+    }
+}
+
+
+
+2)
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+ class Arithmetic 
+{int add(int a,int b)
+{
+    return a+b;
+    
+}} 
+ class Adder extends Arithmetic
+{int add(int a,int b)
+  {
+    return a+b;
+    
+}
+}
+
+ 
+//Write your code here
+
+public class Solution{
+    public static void main(String []args){
+        // Create a new Adder object
+        Adder a = new Adder();
+        
+        // Print the name of the superclass on a new line
+        System.out.println("My superclass is: " + a.getClass().getSuperclass().getName());	
+        
+        // Print the result of 3 calls to Adder's `add(int,int)` method as 3 space-separated integers:
+        System.out.print(a.add(10,32) + " " + a.add(10,3) + " " + a.add(10,10) + "\n");
+     }
+}
+
+3)
+import java.util.*;
+abstract class Book{
+	String title;
+	abstract void setTitle(String s);
+	String getTitle(){
+		return title;
+	}
+}
+class MyBook extends Book{
+    void setTitle(String s)
+    {this.title=s;}
+    
+}
+
+
+public class Main{
+	
+	public static void main(String []args){
+		//Book new_novel=new Book(); This line prHMain.java:25: error: Book is abstract; cannot be instantiated
+		Scanner sc=new Scanner(System.in);
+		String title=sc.nextLine();
+		MyBook new_novel=new MyBook();
+		new_novel.setTitle(title);
+		System.out.println("The title is: "+new_novel.getTitle());
+      	sc.close();
+		
+	}
+}
+
+4)
+import java.util.*;
+interface AdvancedArithmetic{
+  int divisor_sum(int n);
+}
+class MyCalculator implements AdvancedArithmetic
+{public int divisor_sum(int n)
+{int sum=0;
+    int num=n;
+    for(int i=1;i<=n;i++)
+    {if(num%i==0)
+    {
+     sum+=i;
+    }   
+    }
+   return sum;
+}}
+
+class Solution{
+    public static void main(String []args){
+        MyCalculator my_calculator = new MyCalculator();
+        System.out.print("I implemented: ");
+        ImplementedInterfaceNames(my_calculator);
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.print(my_calculator.divisor_sum(n) + "\n");
+      	sc.close();
+    }
+    /*
+     *  ImplementedInterfaceNames method takes an object and prints the name of the interfaces it implemented
+     */
+    static void ImplementedInterfaceNames(Object o){
+        Class[] theInterfaces = o.getClass().getInterfaces();
+        for (int i = 0; i < theInterfaces.length; i++){
+            String interfaceName = theInterfaces[i].getName();
+            System.out.println(interfaceName);
+        }
+    }
+}
+
